9a43e3d8f07d1287fa986a8fa5cbd228b73425ed
sylviovieira <sylviovieira@gmail.com>
2023-06-22 15:06:24 -0300

    Ajustados os campos conforme solicitação da Laura, como CM, Kg etc em RN e Ummes, bem como o botao copiar

--- js/copiar.js
+++ js/copiar.js
@@ -1,10 +1,17 @@
-function copiarFormulario() {
-  var formulario = document.getElementById('formulario');
-  var inputTemporario = document.createElement('input');
-  inputTemporario.value = formulario.outerHTML;
-  document.body.appendChild(inputTemporario);
-  inputTemporario.select();
-  document.execCommand('copy');
-  document.body.removeChild(inputTemporario);
-  alert('O formulário foi copiado para a área de transferência!');
-}
+$( document ).ready(function() {
+  $("#btnSalvar").click(function(){
+
+    let textoCopiar = '';
+
+    /*
+      pega todos os campos do formulário id="dados", transforma num JSON
+      percorre o array com os dados do formulário e limpa ele para como será guardado no textoCopiar
+    */
+    $.each($('#dados').serializeArray(), function() {
+      textoCopiar = textoCopiar+`${this.name.toUpperCase()}: ${this.value} ;`;
+    });
+
+    //joga o textoCopiar para a memória
+    navigator.clipboard.writeText(textoCopiar);
+  });
+});
