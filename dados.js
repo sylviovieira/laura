5d8a06806d405363d09be678d69ef6e0cf33acef
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2023-09-21 08:28:47 -0300

    primeiros testes de formatação de dados

--- dados.js
+++ dados.js
@@ -1,16 +1,20 @@
 $( document ).ready(function() {
   $("#btnSalvar").click(function(){
 
-    let textoCopiar = '';
+    // titulo principal
+    let textoCopiar = 'Anamnese - Dados Coletados: \n\n';
+
+    // subtitulo seção de identificação
+    textoCopiar += 'Identificação do Recém nascido: \n';
 
     /*
       pega todos os campos do formulário id="dados", transforma num JSON
       percorre o array com os dados do formulário e limpa ele para como será guardado no textoCopiar
     */
     $.each($('#dados').serializeArray(), function() {
-      textoCopiar = textoCopiar+`${this.name.toUpperCase()}: ${this.value} ;`;
+      textoCopiar = textoCopiar+`${this.name.chartAt(0).toUpperCase() + this.name.slice(1)}: ${this.value}\n\n`;
     });
-
+    
     //joga o textoCopiar para a memória
     navigator.clipboard.writeText(textoCopiar);
   });
