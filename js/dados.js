5533d6ea52320cfa8b5bf0c14d479becad065395
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2023-09-21 09:04:24 -0300

    Update dados.js

--- js/dados.js
+++ js/dados.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
   $("#btnSalvar").click(function() {
 
-    let textoCopiar = ''; // Título principal
+    let textoCopiar = 'Anamnese - Dados Coletados:\n\n'; // Título principal
 
-    $.each($('#dados').serializeArray(), function() {
-      // Adiciona um título para cada campo e formata o valor em um novo parágrafo
-      textoCopiar += `${this.name.charAt(0).toUpperCase() + this.name.slice(1)}: ${this.value}\n`;
-    });
+    // Função para formatar os dados de cada classe
+    function formatarDadosPorClasse(classe) {
+      const elementos = $(`.${classe} input:checked, .${classe} input[type=text], .${classe} input[type=number]`);
+      elementos.each(function() {
+        const nome = $(this).attr('name');
+        const valor = $(this).val();
+        if (valor && valor.trim() !== '') {
+          textoCopiar += `${nome.charAt(0).toUpperCase() + nome.slice(1)}: ${valor}\n`;
+        }
+      });
+      textoCopiar += '\n';
+    }
+
+    // Formatar os dados de cada classe
+    formatarDadosPorClasse('Identificação');
+    formatarDadosPorClasse('estadoGeral');
+    formatarDadosPorClasse('estadoDaPele');
+    formatarDadosPorClasse('krammer');
+    formatarDadosPorClasse('millium');
+    formatarDadosPorClasse('cabeca');
+    formatarDadosPorClasse('linfonodos');
 
     // Copia os valores formatados para a memória
     navigator.clipboard.writeText(textoCopiar);
