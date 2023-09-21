608ce44c85f22d1749ebe2daa0cd5d5da656d297
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2023-09-21 09:36:36 -0300

    Update dados.js

--- js/dados.js
+++ js/dados.js
@@ -1,32 +1,10 @@
-$(document).ready(function() {
-  $("#btnSalvar").click(function() {
-    let textoCopiar = 'Anamnese - Dados Coletados:\n\n'; // Título principal
+$("#btnSalvar").click(function() {
 
-    // Função para formatar os dados de cada classe
-    function formatarDadosPorClasse(classe) {
-      const elementos = $(`.${classe} input:checked, .${classe} input[type=text], .${classe} input[type=number]`);
-      let dadosClasse = '';
-      elementos.each(function() {
-        const nome = $(this).attr('name');
-        const valor = $(this).val();
-        if (valor && valor.trim() !== '') {
-          dadosClasse += `${nome.charAt(0).toUpperCase() + nome.slice(1)}: ${valor}  `;
-        }
-      });
-      if (dadosClasse) {
-        textoCopiar += dadosClasse + '\n';
-      }
-    }
+    let textoCopiar = ''; // Título principal
 
-    // Coletar todas as classes do formulário
-    const classesDoFormulario = [];
-    $('#dados div[class]').each(function() {
-      classesDoFormulario.push($(this).attr('class'));
-    });
-
-    // Iterar sobre cada classe e formatar os dados
-    classesDoFormulario.forEach(classe => {
-      formatarDadosPorClasse(classe);
+    $.each($('#dados').serializeArray(), function() {
+      // Adiciona um título para cada campo e formata o valor em um novo parágrafo
+      textoCopiar += `${this.name.charAt(0).toUpperCase() + this.name.slice(1)}: ${this.value}\n`;
     });
 
     // Copia os valores formatados para a memória
